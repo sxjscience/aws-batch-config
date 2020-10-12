06be2ad36a971cd651cf8f1f5c71accecdb97462
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-11 19:06:42 -0700

    Create config_batch.py

---
+++ config_batch.py
@@ -0,0 +1,35 @@
+import boto3
+import logging
+
+
+def basic_configuration():
+    ret = dict()
+    ret['jobDefinitionName'] = 'gluon-nlp-g4-12dn'
+    ret['job_type'] = 'container'
+    ret['containerProperties'] = {
+        'image': '747303060528.dkr.ecr.us-east-1.amazonaws.com/gluon-nlp-1:gpu-ci-latest',
+        'vcpus': 48,
+        'memory': 180000,
+        'command': ["./gluon_nlp_job.sh",
+                    "Ref::SOURCE_REF",
+                    "Ref::WORK_DIR",
+                    "Ref::COMMAND",
+                    "Ref::SAVED_OUTPUT",
+                    "Ref::SAVE_PATH",
+                    "Ref::REMOTE"],
+        "resourceRequirements": [
+            {
+                "type": "GPU",
+                "value": "4"
+            }
+        ],
+        "linuxParameters": {
+            'sharedMemorySize': 2000
+        }
+    }
+    return ret
+
+
+client = boto3.client('batch')
+response = client.register_job_definition(**basic_configuration())
+print(response)
