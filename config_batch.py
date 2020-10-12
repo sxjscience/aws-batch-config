ca04b7d1aa1d1b7558ad60b0d8bef34f66775512
Xingjian Shi <xshiab@connect.ust.hk>
2020-10-11 22:52:15 -0700

    Update config_batch.py

--- config_batch.py
+++ config_batch.py
@@ -60,7 +60,7 @@ def generate_job_definition(instance_type):
         "resourceRequirements": [
             {
                 "type": "GPU",
-                "value": str(instance_type['num_gpu'])
+                "value": str(instance_info['num_gpu'])
             }
         ],
         "privileged": True
