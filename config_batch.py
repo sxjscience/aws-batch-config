8376ba59e76fa1572d2a626e58353a28bf7b9ecd
yinweisu <wyin@ucdavis.edu>
2020-10-13 16:53:35 -0700

    Updated mem config

--- config_batch.py
+++ config_batch.py
@@ -12,35 +12,35 @@ project = args.project
 
 instance_info_mapping = {
     # P2 Instance
-    'p2.xlarge': {'vcpus': 4, 'memory': 15000, 'num_gpu': 1},
-    'p2.8xlarge': {'vcpus': 32, 'memory': 30000, 'num_gpu': 8},
-    'p2.16xlarge': {'vcpus': 64, 'memory': 62000, 'num_gpu': 16},
+    'p2.xlarge': {'vcpus': 4, 'memory': 50000, 'num_gpu': 1},
+    'p2.8xlarge': {'vcpus': 32, 'memory': 450000, 'num_gpu': 8},
+    'p2.16xlarge': {'vcpus': 64, 'memory': 700000, 'num_gpu': 16},
 
     # G4 Instance
-    'g4dn.xlarge': {'vcpus': 4, 'memory': 15000, 'num_gpu': 1},
-    'g4dn.2xlarge': {'vcpus': 8, 'memory': 30000, 'num_gpu': 1},
-    'g4dn.4xlarge': {'vcpus': 16, 'memory': 62000, 'num_gpu': 1},
-    'g4dn.8xlarge': {'vcpus': 32, 'memory': 126000, 'num_gpu': 1},
-    'g4dn.12xlarge': {'vcpus': 48, 'memory': 190000, 'num_gpu': 4},
+    'g4dn.xlarge': {'vcpus': 4, 'memory': 13000, 'num_gpu': 1},
+    'g4dn.2xlarge': {'vcpus': 8, 'memory': 27000, 'num_gpu': 1},
+    'g4dn.4xlarge': {'vcpus': 16, 'memory': 55000, 'num_gpu': 1},
+    'g4dn.8xlarge': {'vcpus': 32, 'memory': 118000, 'num_gpu': 1},
+    'g4dn.12xlarge': {'vcpus': 48, 'memory': 180000, 'num_gpu': 4},
 
     # P3 Instance
-    'p3.2xlarge': {'vcpus': 8, 'memory': 59000, 'num_gpu': 1},
-    'p3.8xlarge': {'vcpus': 32, 'memory': 240000, 'num_gpu': 4},
-    'p3.16xlarge': {'vcpus': 64, 'memory': 485000, 'num_gpu': 8},
-    'p3.24xlarge': {'vcpus': 96, 'memory': 766000, 'num_gpu': 8},
+    'p3.2xlarge': {'vcpus': 8, 'memory': 55000, 'num_gpu': 1},
+    'p3.8xlarge': {'vcpus': 32, 'memory': 220000, 'num_gpu': 4},
+    'p3.16xlarge': {'vcpus': 64, 'memory': 450000, 'num_gpu': 8},
+    'p3.24xlarge': {'vcpus': 96, 'memory': 700000, 'num_gpu': 8},
 
     # C4 Instance
-    'c4.2xlarge': {'vcpus': 8, 'memory': 13000, 'num_gpu': 0},
-    'c4.4xlarge': {'vcpus': 16, 'memory': 28000, 'num_gpu': 0},
-    'c4.8xlarge': {'vcpus': 36, 'memory': 130000, 'num_gpu': 0},
+    'c4.2xlarge': {'vcpus': 8, 'memory': 12000, 'num_gpu': 0},
+    'c4.4xlarge': {'vcpus': 16, 'memory': 25000, 'num_gpu': 0},
+    'c4.8xlarge': {'vcpus': 36, 'memory': 50000, 'num_gpu': 0},
 
     # C5 Instance
-    'c5.2xlarge': {'vcpus': 8, 'memory': 14000, 'num_gpu': 0},
-    'c5.4xlarge': {'vcpus': 16, 'memory': 30000, 'num_gpu': 0},
-    'c5.9xlarge': {'vcpus': 36, 'memory': 70000, 'num_gpu': 0},
-    'c5.12xlarge': {'vcpus': 48, 'memory': 94000, 'num_gpu': 0},
-    'c5.18xlarge': {'vcpus': 72, 'memory': 142000, 'num_gpu': 0},
-    'c5.24xlarge': {'vcpus': 96, 'memory': 190000, 'num_gpu': 0},
+    'c5.2xlarge': {'vcpus': 8, 'memory': 13000, 'num_gpu': 0},
+    'c5.4xlarge': {'vcpus': 16, 'memory': 25000, 'num_gpu': 0},
+    'c5.9xlarge': {'vcpus': 36, 'memory': 62000, 'num_gpu': 0},
+    'c5.12xlarge': {'vcpus': 48, 'memory': 86000, 'num_gpu': 0},
+    'c5.18xlarge': {'vcpus': 72, 'memory': 124000, 'num_gpu': 0},
+    'c5.24xlarge': {'vcpus': 96, 'memory': 170000, 'num_gpu': 0},
 }
 
 image_tag_mapping = {
